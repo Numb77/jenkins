aceafcb19317caeb87c267ca8d1f259a559d22e3
Amit Miron <amitmiron88@gmail.com>
2022-11-12 04:59:28 -0800

    Added flaskdocker.groovy

---
+++ job-dsl/flaskdocker.groovy
@@ -0,0 +1,25 @@
+job('Flask Docker Build&Push') {
+    scm {
+        git('git://github.com/Numb77/elbit-test.git') {  node ->
+            node / gitConfigName('Numb77')
+            node / gitConfigEmail('amitmiron77@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        flask('flask')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('numb77/job_1')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('Dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
